7727f4519dde4d8e0d0852f746274e88cd5ea92b
KotyshevaYulya <yulyaxd07@gmail.com>
2024-10-23 14:56:41 +0200

    hw7

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -42,6 +43,7 @@ export default function setupServer() {
     app.use(router);
 
     app.use('/auth/uploads', express.static(UPLOAD_DIR));
+      app.use('/api-docs', swaggerDocs());
 
     app.use('*', notFoundHandler);
 
